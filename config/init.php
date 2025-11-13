ac5e07e8b6318ac07fe9814a4a284bde0f12fd0e
Sam <samananiascases@gmail.com>
2025-11-13 20:35:07 +0800

    try fixing some errors

--- config/init.php
+++ config/init.php
@@ -1,6 +1,16 @@
 <?php
 if (session_status() == PHP_SESSION_NONE) session_start();
-$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
-$host = $_SERVER['HTTP_HOST'] . '/myPortfolio/';
+
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https://' : 'http://';
+$host = $_SERVER['HTTP_HOST'];
+
+// If running locally (e.g. localhost), include your folder
+if (strpos($host, 'localhost') !== false) {
+    $host .= '/myPortfolio/';
+} else {
+    // On Wasmer (or any live server), use root path
+    $host .= '/';
+}
+
 define('BASE_URL', $protocol . $host);
 ?>
